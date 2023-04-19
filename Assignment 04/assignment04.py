5a9f5dcc58ef95d9cbd78bb438946f45ccbe4d13
Julia Marcolan <juliamarcolan@usp.br>
2023-04-19 14:53:13 -0300

    FIX: Correct version of assignment 04 aproved by the local run codes

--- Assignment 04/assignment04.py
+++ Assignment 04/assignment04.py
@@ -8,64 +8,137 @@ Task: The objective of this task is to create and execute the Flood Fill Algorit
 and detecting connected components within an image. The algorithm must be capable of producing the pixels belonging to the region’s 
 connected components as output. 
 """
-
-def flood_fill_4n(x, y, img, target_color): 
-    """
-    
-    """
-    M,N = img.shape
-
-    if target_color == 1: 
-        fill_color = 0 
-    else: 
-        fill_color = 1
-    
-    if ( x < 0 or x > M - 1):
-        return
-
-    if (y < 0 or y > N - 1):
-        return 
-
-    if img[x][y] != target_color: 
-        return
-    else: 
-        img[x][y] = fill_color
-
-    flood_fill_4n(x+1, y, img, target_color)
-    flood_fill_4n(x -1, y, img, target_color)
-    flood_fill_4n(x, y+1, img, target_color)
-    flood_fill_4n(x, y-1, img, target_color)
-    
-
-#--------------------------------------------------------------------------------------------------------------------------
 #Importing the necessary libraries. 
 import numpy as np 
 import imageio.v2 as imageio
 import matplotlib.pyplot as plt
 
+def flood_fill_4n(x, y, img, target_color, coords_list): 
+    """
+        Recursive implementation of 4-connected neighborhood flood fill algorithm. The algorithm  searches all nodes 
+        in the matrix that are connected to the initial point by a path of the target color and modifies them to the 
+        replacement color. The function has no returns and the args img and coords_list are passed as reference. 
+        Args: 
+            - x: x coordinate from the seed pixel. 
+            - y: y  coordinate from the seed pixel. 
+            - img: Binary image to be painted. 
+            - target_color: initial color from the seed pixel.
+            - coords_list: List that stores the pixels where the color was change. 
+        Returns: 
+            ...  
+    """
+    M,N = img.shape
+    
+    # Selecting the target and the fill colors 
+    fill_color = 1 - target_color
+    
+    if ( x < 0 or x > M - 1) or (y < 0 or y > N - 1):
+        return
+
+    # Changing (or not) the pixel color
+    if img[x][y] != target_color: 
+        return
+    else: 
+        img[x][y] = fill_color
+        coords_list.append([x,y])
+
+    # Recursive call from the 4-neighbors
+    flood_fill_4n(x+1, y, img, target_color, coords_list)
+    flood_fill_4n(x-1, y, img, target_color, coords_list)
+    flood_fill_4n(x, y+1, img, target_color, coords_list)
+    flood_fill_4n(x, y-1, img, target_color, coords_list)
+
+def flood_fill_8n(x, y, img, target_color, coords_list): 
+    """
+        Recursive implementation of 8-connected neighborhood flood fill algorithm. The algorithm  searches all nodes 
+        in the matrix that are connected to the initial point by a path of the target color and modifies them to the 
+        replacement color. The function has no returns and the args img and coords_list are passed as reference. 
+        Args: 
+            - x: x coordinate from the seed pixel. 
+            - y: y  coordinate from the seed pixel. 
+            - img: Binary image to be painted. 
+            - target_color: initial color from the seed pixel.
+            - coords_list: List that stores the pixels where the color was change. 
+        Returns: 
+            ...  
+    """
+    M,N = img.shape
+    
+    # Selecting the target and the fill colors 
+    fill_color = 1 - target_color
+    
+    if ( x < 0 or x > M - 1) or (y < 0 or y > N - 1):
+        return
+
+    # Changing (or not) the pixel color
+    if img[x][y] != target_color: 
+        return
+    else: 
+        img[x][y] = fill_color
+        coords_list.append([x,y])
+
+    # Recursive call from the 4-neighbors
+    flood_fill_8n(x+1, y, img, target_color, coords_list)
+    flood_fill_8n(x-1, y, img, target_color, coords_list)
+    flood_fill_8n(x, y+1, img, target_color, coords_list)
+    flood_fill_8n(x, y-1, img, target_color, coords_list)
+    flood_fill_8n(x+1, y+1, img, target_color, coords_list)
+    flood_fill_8n(x+1, y-1, img, target_color, coords_list)
+    flood_fill_8n(x-1, y-1, img, target_color, coords_list)
+    flood_fill_8n(x+1, y+1, img, target_color, coords_list)
+
+#--------------------------------------------------------------------------------------------------------------------------
+
 #STEP 1: Read parameters (Binary image, Seed pixel x coordinate, Seed pixel y coordinate, Connectivity) and load binary image
 
 #Reading the binary image 
-#filename =  input().rstrip()
-img = (imageio.imread('C:/Users/julia/Documents/ImageProcessing/Assignment 04/0.tiff') > 127).astype(np.uint8)
+filename =  input().rstrip()
+img = (imageio.imread(filename) > 127).astype(np.uint8)
+
+# Forcing the image in an np.array to make it easier to pass the variable as a reference to the function 
 img = np.array(img) 
-#img = np.array([[0,0,0], [1,0,1], [1,1,0]])
+
+#making a copy just for testing and comparing
+img_save = (imageio.imread(filename) > 127).astype(np.uint8)
 
 #Reading Seed pixel x coordinate
-#x = int(input().rstrip()) 
-x = 1
-#Reading Seed pixel y coordinate
-#y = int(input().rstrip()) 
-y = 1
-#Reading Connectivity
-#c = input().rstrip() 
-c = 4  
-target_color = img[x][y]
-flood_fill_4n(x,y, img, target_color)
+x = int(input().rstrip()) 
 
-#print(img)
-#Just  for testing
-plt.figure(figsize=(12,10))
-plt.subplot(121); plt.imshow(img, cmap ='gray')
-#plt.subplot(122); plt.imshow(img2, cmap ='gray')
-plt.show()
+#Reading Seed pixel y coordinate
+y = int(input().rstrip()) 
+
+#Reading Connectivity
+c = int(input().rstrip()) 
+
+# Creating a list to store the coordinates of the modified pixels
+coords_list = [] 
+
+# Selecting the correct connectivity value
+if c == 4: 
+    # Setting the correct start color in the seed pixel 
+    target_color = img[x][y]
+    flood_fill_4n(x, y, img, target_color, coords_list)
+ 
+elif c == 8: 
+    #Setting the correct start color in the seed pixel 
+    target_color = img[x][y]
+    flood_fill_8n(x, y, img, target_color, coords_list)
+else: 
+    print("Invilid Connectivity value ")
+
+
+# Printing the coordinates of the modified pixels
+coords_list.sort()
+
+str_list = []
+for i in range(len(coords_list)): 
+    str_aux  = f"({coords_list[i][0]} {coords_list[i][1]})"
+    str_list.append(str_aux)
+
+print(*str_list, sep = " ")
+
+#Just for testing 
+#plt.figure(figsize=(12,10))
+#plt.subplot(121); plt.imshow(img_save, cmap ='gray')
+#plt.subplot(122); plt.imshow(img, cmap ='gray')
+#plt.show()
