138ac0bc7c16625cf1e528f539d267112b4a29bc
Julia Marcolan <juliamarcolan@usp.br>
2023-04-23 00:16:13 -0300

    ADD: RMSE function

--- Assignment 02/assignment02.py
+++ Assignment 02/assignment02.py
@@ -7,6 +7,11 @@
 Task: The objective of this exercise is to deepen the students understanding of the frequency domain of images and how to use
 Fourier Transform and filtering techniques to match the expected results. 
 """
+#Importing the necessary libraries. 
+import numpy as np 
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+
 def Ideal_Low_pass():
     pass
 
@@ -28,21 +33,30 @@ def Butterworth_low_pass():
 def Butterworth_high_pass():
     pass 
 
-def Butterworth_band_reject():
-    pass 
+def RMSE(img_H, img_R): 
+    """
+        The function calculates the root mean square (RMSE) value between the restored image (img_R) obteined after the filter application 
+        and a reference/expectede image (img_H)
+        
+        Args: 
+            - img_H: Expected image. 
+            - img_R: Restored image.
+        Return: 
+            - RMSE_value: root mean square (RMSE) value between the images.
+    """
 
-def Butterworth_band_pass():
-    pass 
+    # gets the size of the input matrix (M x N). Obs: H and img_R should have the same size 
+    M, N = img_H.shape
 
-def RMSE():
-    pass 
+    sum_squares = np.sum(np.square(img_H - img_R))
+
+    RMSE_value = np.sqrt(sum_squares/(M*N))
+
+    return(RMSE_value)
+
 #--------------------------------------------------------------------------------------------------------------------------
-#Importing the necessary libraries. 
-import numpy as np 
-import imageio.v2 as imageio
-import matplotlib.pyplot as plt
 
-#STEP 1: Read the parameters (Input image I, Expected image H, Filter index i ∈ [0,1,2,3,4,5,6,7,8], Filter Parameters respective to 
+#STEP 1: Read the parameters (Input image I, Expected image H, Filter index i ∈ [0,1,2,3,4,5,6], Filter Parameters respective to 
 #each index)
 
 #Reading the input image I
@@ -53,33 +67,44 @@ img_I = imageio.imread(filenameI)
 filenameH = input().rstrip()
 img_H = imageio.imread(filenameH)
 
-#Reafing the filter index 
-index = input().rstrip()
 
-#Parameters respective to each index
+#Reafing the filter index 
+index = int(input().rstrip())
+
+#Parameters respecindex == 0: tive to each index
+
 if index == 0: 
-    r = input().rstrip()
-elif index == 1: 
-    r = input().rstrip()
+    # Ideal Low-pass
+    r = int(input().rstrip())
+elif index == 0: 
+    # Ideal High-pass
+    r = int(input().rstrip())
 elif index == 2: 
-    r1 = input().rstrip()
-    r2 = input().rstrip()
-elif index == 3: 
-    pass 
+    # Ideal Band-pass
+    r1 = int(input().rstrip())
+    r2 = int(input().rstrip())
+elif index == 3 : 
+    #Laplacian low-pass
+    pass
 elif index == 4: 
-    sigma1 = input().rstrip()
-    sigma2 = input().rstrip()
+    # Gaussian Low-pass
+    sigma1 = int(input().rstrip())
+    sigma2 = int(input().rstrip())
 elif index == 5: 
-    D0 = input().rstrip()
-    N = input().rstrip()
+    # Butterworth low-pass
+    D0 = int(input().rstrip())
+    N = int(input().rstrip())
 elif index == 6: 
-    D0 = input().rstrip()
-    N = input().rstrip()
-elif index == 7: 
-    D0 = input().rstrip()
-    N = input().rstrip()   
-elif index == 8: 
-    D0 = input().rstrip()
-    D1 = input().rstrip()
-    N1 = input().rstrip()
-    N2 = input().rstrip()
+    # Butterworth high-pass
+    D0 = int(input().rstrip())
+    N = int(input().rstrip())
+else: 
+    print("Index not valid")
+
+
+
+#Just  for testing
+#plt.figure(figsize=(12,10))
+#plt.subplot(121); plt.imshow(img_I, cmap ='gray')
+#plt.subplot(122); plt.imshow(img_H, cmap ='gray')
+#plt.show()
